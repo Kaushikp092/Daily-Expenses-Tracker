61abb0445c31568be2243dcbdf6f52a20e08f578
kaushikp092 <kaushikpatil092@gmail.com>
2026-03-14 15:16:53 +0530

    main server file

---
+++ Backend/server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require('./config/db');
+const port = process.env.PORT;
+
+const app = express();
+
+// connecting frontend to backend
+app.use(cors({origin: process.env.CLIENT_URL}));
+
+// mongoDb mongoose connection
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", require("./routes/authRouter"));
+app.use("/api/expenses", require("./routes/expenseRouter"));
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
