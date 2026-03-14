5c7778e697848582ab5b72d323e9ab7bdb87cca0
kaushikp092 <kaushikpatil092@gmail.com>
2026-03-14 15:18:30 +0530

    login and register routes

---
+++ Backend/routes/authRouter.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/Users");
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+	try {
+		const { name, email, password } = req.body;
+
+		const hashedPassword = await bcrypt.hash(password, 10);
+
+		const user = new User({
+			name,
+			email,
+			password: hashedPassword,
+		});
+
+		await user.save();
+
+		res.status(201).json({ message: "User Registered" });
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+});
+
+router.post("/login", async (req, res) => {
+	try {
+		const { email, password } = req.body;
+
+		const user = await User.findOne({ email });
+
+		if (!user) return res.status(404).json({ message: "User Not Found" });
+
+		const isMatch = await bcrypt.compare(password, user.password);
+
+		if (!isMatch) return res.status(400).json({ message: "Invalid Password" });
+
+		const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY, {
+			expiresIn: "1d",
+		});
+
+		res.status(200).json({
+			token,
+			user: {
+				id: user._id,
+				username: user.username,
+				email: user.email,
+			},
+		});
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+});
+
+module.exports = router;
