b0f92c282fd7d310e969d03b3d70a06f146a2b53
kaushikp092 <kaushikpatil092@gmail.com>
2026-03-19 23:48:06 +0530

    change in expense route

--- Backend/routes/expenseRouter.js
+++ Backend/routes/expenseRouter.js
@@ -8,37 +8,56 @@ router.post("/", authMiddleware, async (req, res) => {
 	try {
 		const { title, amount, category } = req.body;
 
-		const expense = new Expense({
+		const expenses = new Expense({
 			title,
 			amount,
 			category,
 			userID: req.user.id,
 		});
 
-		await expense.save();
+		await expenses.save();
 
-		res.status(200).json({ expense });
+		res.status(200).json({ expenses });
 	} catch (err) {
-		res.status(500).json({ message: err.message });
+		return res.status(500).json({ message: err.message });
 	}
 });
 
 router.get("/", authMiddleware, async (req, res) => {
 	try {
-        console.log('Logged User ID:', req.user.id);
-		const expense = await Expense.find({ userId: req.user.id });
-		res.status(200).json({ expense });
+		const expenses = await Expense.find({ userID: req.user.id }).populate(
+			"userID",
+			"name",
+		);
+		return res.status(200).json({ expenses });
 	} catch (err) {
-		res.status(500).json({ message: err.message });
+		return res.status(500).json({ message: err.message });
+	}
+});
+
+router.put("/:id", authMiddleware, async (req, res) => {
+	try {
+		const updatedExpense = await Expense.findByIdAndUpdate(
+			req.params.id,
+			req.body,
+			{ returnDocument: "after" },
+		);
+
+		if (!updatedExpense)
+			return res.status(404).json({ message: "Expense Not Found" });
+
+		return res.status(200).json({ expense: updatedExpense });
+	} catch (err) {
+		return res.status(500).json({ message: err.message });
 	}
 });
 
 router.delete("/:id", authMiddleware, async (req, res) => {
 	try {
-		await Expense.findOneAndDelete({_id: req.params.id, userId: req.user.id});
-		res.status(200).json({ message: "Expense Deleted" });
+		await Expense.findOneAndDelete({ _id: req.params.id, userID: req.user.id });
+		return res.status(200).json({ message: "Expense Deleted" });
 	} catch (err) {
-		res.status(500).json({ message: err.message });
+		return res.status(500).json({ message: err.message });
 	}
 });
 
